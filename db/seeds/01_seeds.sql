d8bf62732693236b516eecaa6361de81cee98bf7
Soyoung <ksyoung3424@gmail.com>
2023-07-21 15:38:22 +0000

    Update 01_seeds.sql with rescue info

--- db/seeds/01_seeds.sql
+++ db/seeds/01_seeds.sql
@@ -1,27 +1,27 @@
-INSERT INTO users (name, email, password) 
+INSERT INTO users (name, email, password)
 VALUES
 ('Sarah Johnson', 'sa@gmail.com', '123'),
 ('Michael Rodriguez', 'mi@gmail.com', '456'),
 ('Emily Lee', 'em@gmail.com', '789');
 
-INSERT INTO categories (category) 
-VALUES 
+INSERT INTO categories (category)
+VALUES
 ('IT');
 
-INSERT INTO resources (title, url, description, url_cover_photo,user_id , category_id)
+INSERT INTO resources (title, url, description, url_cover_photo, user_id , category_id)
 VALUES
-('LHL page - 1', 'https://www.lighthouselabs.ca/en/web-development', 'fm', 'Full Stack Development', 1, 1),
-('LHL page - 2', 'https://www.lighthouselabs.ca/en/data-science', 'gm', 'Data Science', 1, 1),
-('LHL page - 3', 'https://www.lighthouselabs.ca/en/cyber-security', 'hom', 'Cyber-security', 1, 1);
+('Save Our Scruff', 'https://www.saveourscruff.org/', 'Save Our Scruff is a non-profit dog rescue and rehome charity located in Toronto and Southern Ontario', 'https://images.squarespace-cdn.com/content/v1/5538679ce4b07179e5b8a295/81ddf636-4539-4ba0-99c8-93975be34b40/004-SOS-Logos-tur.png?format=1500w', 1, 1),
+('VOKRA', 'https://www.vokra.ca/', 'VOKRA is a no-kill, registered non-profit cat rescue organization', 'https://images.squarespace-cdn.com/content/v1/5bbba72a2727be42cf12b47a/a42eb64b-6b5a-4740-9336-88e70711b9e7/home+banner.jpg?format=2500w', 1, 1),
+('Horse Heroes Alberta', 'https://horseheroesalberta.com/', 'homHorse Heroes Alberta Rescue Association is not your typical horse rescue', 'https://img1.wsimg.com/isteam/ip/c2f96be2-3201-4a50-97c7-e8eeafe72c2f/20230515_090747.jpg/:/cr=t:0%25,l:0.12%25,w:99.77%25,h:100%25/rs=w:2480,h:1240,cg:true', 1, 1);
 
 INSERT INTO feedbacks (comment, rating, user_id, resource_id)
 VALUES
-('Life changing coding bootcamp', 5, 1, 1),
-('Career-focused tech training', 4, 1, 1),
-('Industry-experienced instructors', 4, 1, 1);
+('Very thorough adoption process. Highly recommended', 5, 1, 1),
+('Amazing organization that saves so many sweet lives', 4, 1, 1),
+('Amazing place to adopt horses', 4, 1, 1);
 
 INSERT INTO likes (user_id, resource_id)
-VALUES 
+VALUES
 (1,1),
 (1,1),
 (1,1);
